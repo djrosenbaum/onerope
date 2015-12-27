981c918586d04518d64b6f965c389e7778c79874
DJ Rosenbaum <dave@djrconcepts.com>
2015-12-26 19:23:35 -0500

    added room name validation

--- games/tic_tac_toe/rooms/js/script.js
+++ games/tic_tac_toe/rooms/js/script.js
@@ -1,15 +1,36 @@
 //create new room button
-$('.create_new_room').on('click', create_room_handler);
-
-function create_room_handler() {
+$('.create_new_room').on('click', function() {
     //get value from input box
     var room_name = $('input.room_name').val();
     console.log('create room: ', room_name);
 
-    //create room and add to list
-    onerope.rooms.create_room(room_name);
-}
+    if ( room_name_is_valid(room_name) ) {
+        //create room and add to list
+        onerope.rooms.create_room(room_name);
+    }
+});
 
+$('input.room_name').on('input', function() {
+    var room_name = $(this).val();
+    var valid_room_name = '';
+
+    for ( var i=0; i<room_name.length; i++ ) {
+        if ( /[a-zA-Z]/.test( room_name[i] ) ) {
+            valid_room_name += room_name[i];
+        }
+    }
+
+    if ( room_name !== valid_room_name ) {
+        $('input.room_name').val(valid_room_name);
+    }
+});
+
+function room_name_is_valid(room_name) {
+    if ( room_name.length < 1 ) {
+        return false;
+    }
+    return true;
+}
 
 $( document ).ready(function() {
     onerope.rooms.add_player();
