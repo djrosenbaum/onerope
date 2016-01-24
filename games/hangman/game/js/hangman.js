81ebb2dd3ec71a422019c0dd9354668744c6516a
DJ Rosenbaum <dave@djrconcepts.com>
2016-01-24 17:43:15 -0500

    entering player name

--- games/hangman/game/js/hangman.js
+++ games/hangman/game/js/hangman.js
@@ -4,3 +4,26 @@ console.log('hangman v4');
 // $('#scrollable').animate({
 //     scrollLeft: 300},
 // 500);
+
+var current_screen = 'enter_name';
+var first_letter = false;
+
+$('.alphabet .letter').on('click', function () {
+    if ( current_screen === 'enter_name'  ) {
+
+        if ( !first_letter ) {
+            first_letter = true;
+            $('.hangman_inner_wrapper .name_wrapper').empty();
+        }
+
+        var letter = $(this).text();
+
+        console.log('letter: ', letter);
+
+        $('.hangman_inner_wrapper .name_wrapper').append(
+            '<div class="letter_underline">' +
+                '<div class="letter">' + letter + '</div>' +
+            '</div>'
+        );
+    }
+});
