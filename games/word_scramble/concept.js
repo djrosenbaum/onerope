e4c128c847eb93d46b3416df5cfcd56a700b35eb
DJ Rosenbaum <djrosenbaum@gmail.com>
2016-03-10 23:15:57 -0500

    was riding on the path train with luke and we came up with a concept idea for a group word scramble game

---
+++ games/word_scramble/concept.js
@@ -0,0 +1,27 @@
+// var countries = ['usa', 'canada', 'england'];
+
+var numbers = [];
+var scrambled_numbers;
+
+var country = 'australia';
+// var random_country = [];
+// var total_letters = country.length;
+
+for ( var i=0; i < country.length; i++ ) {
+
+	numbers.push(i);
+
+	//var random_number = Math.ceil(Math.random() * total_letters);
+
+}
+
+
+for ( var i=0; i < country.length; i++ ) {
+
+	var random_number = Math.ceil(Math.random() * numbers.length);
+
+	scrambled_numbers[i] = numbers[random_number];
+
+	numbers
+
+}
